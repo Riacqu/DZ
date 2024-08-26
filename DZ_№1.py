a219e6acfb9ff5b043ab2f2b44ad5accc51f333b
Riacqu <kozhin72@gmail.com>
2024-08-26 05:28:40 +0300

    Add files via upload

---
+++ DZ_№1.py
@@ -0,0 +1,25 @@
+''' Задание 1.
+Выведите на экран надпись To be or not to be на разных строках'''
+
+# print("To be\nor not\nto be")
+
+
+'''Задание 2.
+Выведите на экран надпись «Life is what happens when you're busy making other
+plans» John Lennon на разных строках. '''
+
+
+# print("«Life is what happens when \n\t\t\t you're busy making \n\t\t\t\t", end='\t ')
+# print("other plans» John Lennon")
+
+'''Задание 3.
+Определите тип данных 10 и '10'
+Результат выведите на экран'''
+
+# print(type(10))
+# print(type('10'))
+
+'''Задание 4.
+Выведите на экран треугольник из звездочек'''
+
+# print('*\n**\n***\n****\n*****')
